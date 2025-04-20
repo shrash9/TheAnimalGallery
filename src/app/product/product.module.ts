acc361ba06a3ed115c477c06eeba443183d5f705
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-04-20 14:32:10 +0530

    Installed angular flex and styled the products to grid

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import {MatCardModule} from '@angular/material/card'
-
+import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import {MatCardModule} from '@angular/material/card'
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    FlexModule
   ]
 })
 export class ProductModule { }
