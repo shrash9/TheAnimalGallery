457412b01b9e45ade1c0479d14611ab1583b9ecf
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-04-21 17:40:31 +0530

    Added cart funtionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatButtonModule} from '@angular/material/button'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import {MatButtonModule} from '@angular/material/button'
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    CartModule
 
   ],
   providers: [],
